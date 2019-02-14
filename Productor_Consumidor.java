a7b0e8368fa46fb8dfc569493ee142bfc658a082
navi931 <47607235+navi931@users.noreply.github.com>
2019-02-13 21:26:01 -0600

    Add files via upload

--- Productor_Consumidor.java
+++ Productor_Consumidor.java
@@ -3,30 +3,81 @@ package productor_consumidor;
 import java.nio.CharBuffer;
 
 public class Productor_Consumidor 
-{public static void main (String [] args) {
-        final int tamaño = 3;
-        int i = 0;
-        CharBuffer buffer = CharBuffer.allocate(tamaño);
+{
+    static int tamaño = 3;
+    static int i = 0;
+    static CharBuffer buffer = CharBuffer.allocate(tamaño);
+    static String text = "Este texto es demasiado pequeño como para explicar un concepto de manera clara";
+    static boolean lleno = false;
+    
+    static class workerWriter extends Thread 
+        {
+          public void run () 
+          {
+              while(true)
+              {
+                    if (!lleno)
+                    {
+                        synchronized (text)
+                        {
+                          for (; (i < tamaño)&&(i<text.length()); i++)
+                          {
+                                char c = text.charAt(i);
+                                buffer.put(c);
+                          }
+                        }
+                    lleno = true;
+                    if(i>=text.length())
+                        break;
+                    }
+              }
+          }
+        }
+    static class workerReader extends Thread 
+    {
+          public void run () 
+          {
+           while(true)
+           {
+            if (lleno)
+            {
+              synchronized (text)
+              {
+                 buffer.flip();
+                 while (buffer.hasRemaining()) 
+                 {
+                  System.out.print(buffer.get());                   
+                 }
+                 buffer.clear();
+              }
+              tamaño=tamaño+3;
+              lleno = false;
+              if(i>=text.length())
+                break;
+            }
+           }
+           System.out.println("");
+          }
+    }
+    public static void main (String [] args) 
+    {
         
-        String text = "Este texto es demasiado pequeño como para explicar un concepto de manera clara";
         System.out.println("Input text: " + text);
- 
-        for (; i < tamaño; i++) {
- 
-            char c = text.charAt(i);
-            buffer.put(c);
-        }
- 
-        int buffPos = buffer.position();
-        System.out.println("Position after data is written into buffer: " + buffPos);
- 
-        buffer.flip();
-         
-        System.out.println("Reading buffer contents:");
-         
-        while (buffer.hasRemaining()) {
- 
-            System.out.println(buffer.get());                   
-        }
+        
+        Thread thread1 = new workerWriter();
+        Thread thread2 = new workerReader();
+
+        thread1.start();
+        thread2.start();
+
+        try 
+           {
+            thread1.join();
+            thread2.join();
+           }
+        catch (InterruptedException e)
+            {
+             System.out.println(e);
+            }
     }
 }
